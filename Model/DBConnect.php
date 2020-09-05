a85a4c76bbed076833718eafdcfa0af3258a6f57
vietptqgcd17158 <55428662+vietptqgcd17158@users.noreply.github.com>
2020-09-05 10:25:19 +0700

    Update DBConnect.php

--- Model/DBConnect.php
+++ Model/DBConnect.php
@@ -1,9 +1,9 @@
 
 <?php
-$server_username = "root"; // thông tin đăng nhập host
-$server_password = ""; // mật khẩu, trong trường hợp này là trống
-$server_host = "localhost"; // host là localhost
-$database = 'mvc'; // database là mvc
+$server_username = "ykuuzflcyqrygr"; // thông tin đăng nhập host
+$server_password = "ba6b1388e76904b06712572a99c424bcf6714e7058a2a8738e147ea22d161207"; // mật khẩu, trong trường hợp này là trống
+$server_host = "ec2-34-232-212-164.compute-1.amazonaws.com"; // host là localhost
+$database = 'd33q1ni6h0nvha'; // database là mvc
 
 // Tạo kết nối đến database dùng mysqli_connect()
 $conn = mysqli_connect($server_host,$server_username,$server_password,$database) or die("không thể kết nối tới database");
